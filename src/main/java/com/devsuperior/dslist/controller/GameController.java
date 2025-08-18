ca22644e171ac792d7aeea1cc2d002395450d031
Nicolas <nicolasrodriguesveloso@gmail.com>
2025-08-18 15:08:48 -0300

    feat: Game, repository, service, controller

---
+++ src/main/java/com/devsuperior/dslist/controller/GameController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dslist.controller;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.service.GameService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GameMinDTO>> findAll() {
+        return ResponseEntity.ok(gameService.findAll());
+    }
+}
---
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dslist.service;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repository.GameRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public List<GameMinDTO> findAll() {
+        List<Game> gameList = gameRepository.findAll();
+        return gameList.stream().map(GameMinDTO::new).toList();
+    }
+}
